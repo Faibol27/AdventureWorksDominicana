eccf46b4216e156a8738fd829934f0190db2fb15
Enel Almonte <enelramon@hotmail.com>
2026-03-16 20:07:35 -0400

    Organiza usings de ShipMethod
    Closes #18

--- AdventureWorksDominicana.Services/ShipMethodService.cs
+++ AdventureWorksDominicana.Services/ShipMethodService.cs
@@ -2,10 +2,7 @@
 using AdventureWorksDominicana.Data.Models;
 using Aplicada1.Core;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace AdventureWorksDominicana.Services;
 
